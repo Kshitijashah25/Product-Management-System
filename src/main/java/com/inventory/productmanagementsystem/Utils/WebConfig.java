c5aa720d1dab95fa8d445a23fc4440a63e5bd347
Kshitijashah25 <kshitijaashah@gmail.com>
2023-05-14 17:20:11 +0530

    WebConfig.java edited

--- src/main/java/com/inventory/productmanagementsystem/Utils/WebConfig.java
+++ src/main/java/com/inventory/productmanagementsystem/Utils/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        WebMvcConfigurer.super.addCorsMappings(registry);
+//        WebMvcConfigurer.super.addCorsMappings(registry);
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
